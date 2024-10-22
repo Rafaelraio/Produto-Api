02c130f568863245572be61023acd55c66e28afc
Rafael Augusto de Souza <rafaelaugustodesouza@MacBook-Air-de-Rafael.local>
2024-10-21 21:54:19 -0300

    Adicionando update, get by id e delete

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const insertProduct = async (descricao, preco, estoque, Adata) => {
     return data
   } catch (error) {
     console.log(error)
-    throw new Error('Erro ao inserir produto: ' + error.message)
+    throw new Error('Erro ao inserir produto')
   }
 }
 
@@ -28,9 +28,44 @@ const getProducts = async () => {
       return products
     } catch (error) {
         console.log(error)
-      throw new Error('Erro ao buscar produtos: ')
+      throw new Error('Erro ao buscar produtos')
     }
   }  
+
+  const getProductById = async (id) => {
+    try {
+      const products = await db.one('SELECT * FROM Produto Where ID=$1',[id])
+      return products
+    } catch (error) {
+        console.log(error)
+      throw new Error('Erro ao buscar produto')
+    }
+  } 
+
+  const deleteProductById = async (id) => {
+    try {
+      const result = await db.result('DELETE FROM Produto Where ID=$1',[id])
+      if (result.rowCount === 0) {
+        throw new Error('Produto não encontrado');
+      }
+      return { message: 'Produto deletado' };
+    } catch (error) {
+        console.log(error)
+      throw new Error('Erro ao deletar produto')
+    }
+  } 
+  const updateProductById = async (id, descricao, preco, estoque, Adata) => {
+    try {
+      const result = await db.result('UPDATE Produto SET DESCRICAO = $2, PRECO = $3, ESTOQUE = $4, DATA = $5 WHERE ID = $1',[id, descricao, preco, estoque, Adata])
+      if (result.rowCount === 0) {
+        throw new Error('Produto não encontrado');
+      }
+      return { message: 'Produto atualizado' };
+    } catch (error) {
+        console.log(error)
+      throw new Error('Erro ao atualizar produto')
+    }
+  } 
 
 app.post('/produto', async (req, res) => {
   const { descricao, preco, estoque, data } = req.body
@@ -41,7 +76,8 @@ app.post('/produto', async (req, res) => {
       produto: novoProduto,
     })
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    console.log(error)
+      throw new Error('Erro ao inserir produto')
   }
 })
 
@@ -50,12 +86,46 @@ app.get('/produto', async (req, res) => {
       const produtos = await getProducts()
       res.status(200).json(produtos)
     } catch (error) {
+        console.log(error)
+        throw new Error('Erro ao atualizar produto')
+    }
+  })
+
+
+  app.get('/produto/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+      const produtos = await getProductById(id)
+      res.status(200).json(produtos)
+    } catch (error) {
+        console.log(error)
+        throw new Error('Erro ao buscar produto')
+    }
+  })
+
+  app.delete('/produto/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+      const produtos = await deleteProductById(id)
+      res.status(200).json(produtos)
+    } catch (error) {
+      res.status(500).json({ message: error.message })
+    }
+  })
+
+  app.put('/produto/:id', async (req, res) => {
+    const { id } = req.params;
+    const {descricao, preco, estoque, data } = req.body;
+    try {
+      const produtos = await updateProductById(id, descricao, preco, estoque, data)
+      res.status(200).json(produtos)
+    } catch (error) {
       res.status(500).json({ message: error.message })
     }
   })
 
 app.get('/', (req, res) => {
-  res.send('Rotas que existem:\n /produto')
+  res.send('Rotas que existem:\n/produto\n/produto/:id')
 })
 
 app.listen(port, () => {
